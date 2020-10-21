a0987f8b7875e0fdb3be6f157f2b28b210d8af5a
bhp <bhp@easv.dk>
2020-10-22 01:24:19 +0200

    Added StudentService.Add + test

--- Services/StudentService.cs
+++ Services/StudentService.cs
@@ -1,8 +1,44 @@
-﻿using System;
+﻿using Castle.Core.Internal;
+using Interfaces;
+using Model;
+using System;
 
 namespace Services
 {
     public class StudentService
     {
+        private IStudentRepository repo;
+
+        public StudentService(IStudentRepository repo)
+        {
+            if (repo == null)
+            {
+                throw new ArgumentException("Missing Student Repository");
+            }
+            this.repo = repo;
+        }
+
+        public void AddStudent(Student s)
+        {
+            if (s == null)
+            {
+                throw new ArgumentException("Missing student");
+            }
+            if (!IsValidStudent(s))
+            {
+                throw new ArgumentException("Invalid student");
+            }
+            repo.Add(s);
+        }
+
+        private bool IsValidStudent(Student s)
+        {
+            return (s.Id > 0
+                && !s.Name.IsNullOrEmpty()
+                && !s.Address.IsNullOrEmpty()
+                && s.ZipCode > 0
+                && !s.PostalDistrict.IsNullOrEmpty()
+                && s.Email != "");
+        }
     }
 }
